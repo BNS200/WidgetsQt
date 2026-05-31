e3a0775f05bf62eb56d925257c6ef8a4aab95b27
BNS200 <nbufalov2005@gmail.com>
2026-05-31 18:33:57 +0700

    Implementation of task2.8

---
+++ widgets/win.cpp
@@ -0,0 +1,38 @@
+#include "win.h"
+#include <QString>
+
+Win::Win(QWidget *parent) : QWidget(parent)
+{
+    this->setWindowTitle(QString::fromUtf8("Счетчик"));
+
+    label1 = new QLabel(QString::fromUtf8("Счет по 1"), this);
+    label2 = new QLabel(QString::fromUtf8("Счет по 5"), this);
+    edit1 = new Counter("0", this);
+    edit2 = new Counter("0", this);
+    calcbutton = new QPushButton("+1", this);
+    exitbutton = new QPushButton(QString::fromUtf8("Выход"), this);
+
+    QHBoxLayout *layout1 = new QHBoxLayout();
+    layout1->addWidget(label1);
+    layout1->addWidget(label2);
+
+    QHBoxLayout *layout2 = new QHBoxLayout();
+    layout2->addWidget(edit1);
+    layout2->addWidget(edit2);
+
+    QHBoxLayout *layout3 = new QHBoxLayout();
+    layout3->addWidget(calcbutton);
+    layout3->addWidget(exitbutton);
+
+    QVBoxLayout *layout4 = new QVBoxLayout(this);
+    layout4->addLayout(layout1);
+    layout4->addLayout(layout2);
+    layout4->addLayout(layout3);
+
+
+    connect(calcbutton, &QPushButton::clicked, edit1, &Counter::add_one);
+
+    connect(edit1, &Counter::tick_signal, edit2, &Counter::add_one);
+
+    connect(exitbutton, &QPushButton::clicked, this, &Win::close);
+}
